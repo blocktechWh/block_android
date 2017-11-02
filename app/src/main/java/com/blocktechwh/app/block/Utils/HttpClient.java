889d4642fffc0db2f92e16393aa4edc846420263
胡义晋 <eagune@huyijindeMacBook-Pro.local>
2017-11-02 17:30:31 +0800

    注册调用

--- app/src/main/java/com/blocktechwh/app/block/Utils/HttpClient.java
+++ app/src/main/java/com/blocktechwh/app/block/Utils/HttpClient.java
@@ -2,19 +2,25 @@ package com.blocktechwh.app.block.Utils;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.Toast;
 
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
+import com.blocktechwh.app.block.Common.App;
 /**
  * Created by eagune on 2017/11/1.
  */
@@ -29,6 +35,32 @@ public class HttpClient {
             .connectTimeout(10, TimeUnit.SECONDS)
             .writeTimeout(10, TimeUnit.SECONDS)
             .readTimeout(100, TimeUnit.SECONDS);
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    public static void post(Object tag, String url, String json, final CallBack callBack) {
+        RequestBody requestBody = RequestBody.create(JSON, json);
+
+        String Cookie_token = PreferencesUtils.getString(App.getContext(), "Cookie_token", UUID.randomUUID().toString());
+        String token = PreferencesUtils.getString(App.getContext(), "Token", "");
+
+        Request request = new Request.Builder()
+                .addHeader("Cookie_token", Cookie_token)
+                .addHeader("Authorization", token)
+                .tag(tag)
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, final IOException e) {
+                handleError(e, callBack);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException { handleResponse(response, callBack); }
+        });
+    }
 
     public static void get(Object tag, String url, HashMap<String, String> map, final CallBack callBack) {
         if (map != null) {
@@ -45,29 +77,40 @@ public class HttpClient {
                 .build();
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
-            public void onFailure(Call call, final IOException e) {
-                System.out.println("onFail+++");
-            }
+            public void onFailure(Call call, final IOException e) { handleError(e, callBack); }
             @Override
-            public void onResponse(final Call call, Response response) throws IOException {
-                String json = response.body().string();
-                json = json.replace("null", "\"\"");
-                final String finalJson = json;
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            JSONObject jsonObject = new JSONObject(finalJson);
-                            callBack.onSuccess(jsonObject);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            callBack.onFailure(4, e.getLocalizedMessage());
-                        }
-                    }
-                });
-            }
+            public void onResponse(final Call call, Response response) throws IOException { handleResponse(response, callBack); }
         });
 
+    }
+
+    private static void handleResponse(Response response, final CallBack callBack) throws IOException {
+        String json = response.body().string();
+        json = json.replace("null", "\"\"");
+        final String finalJson = json;
+        System.out.println(finalJson);
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JSONObject jsonObject = new JSONObject(finalJson);
+                    callBack.onSuccess(jsonObject);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    callBack.onFailure(4, e.getLocalizedMessage());
+                }
+            }
+        });
+    }
+
+    private static void handleError(final IOException e, final CallBack callBack) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("e.getLocalizedMessage() = " + e.getLocalizedMessage());
+                callBack.onFailure(4, e.getLocalizedMessage());
+            }
+        });
     }
 
 }
