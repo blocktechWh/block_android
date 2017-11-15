67c785c5a12b564465484e79a57bc380032d0bfa
UncleZheng1 <33140954+UncleZheng1@users.noreply.github.com>
2017-11-15 12:42:52 +0800

    fix bug

--- app/src/main/java/com/blocktechwh/app/block/Utils/CallBack.java
+++ app/src/main/java/com/blocktechwh/app/block/Utils/CallBack.java
@@ -1,11 +1,5 @@
 package com.blocktechwh.app.block.Utils;
 
-import android.text.TextUtils;
-import android.widget.Toast;
-
-import com.blocktechwh.app.block.Common.App;
-
-import com.alibaba.fastjson.JSONObject;
 import com.blocktechwh.app.block.Common.ErrorTip;
 
 /**
@@ -16,13 +10,13 @@ public abstract class CallBack<T>{
     public abstract void onSuccess(T result);
 
     public void onFailure(int errorType, String message) {
-        if (errorType != 0) {
-            if(!TextUtils.isEmpty(message)){
-                Toast.makeText(App.getContext(), message, Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            Toast.makeText(App.getContext(), "系统异常！", Toast.LENGTH_SHORT).show();
-        }
+//        if (errorType != 0) {
+//            if(!TextUtils.isEmpty(message)){
+//                Toast.makeText(App.getContext(), message, Toast.LENGTH_SHORT).show();
+//            }
+//        } else {
+//            Toast.makeText(App.getContext(), "系统异常！", Toast.LENGTH_SHORT).show();
+//        }
         System.out.println("errorType = [" + errorType + "], message = [" + message + "]");
     }
 
